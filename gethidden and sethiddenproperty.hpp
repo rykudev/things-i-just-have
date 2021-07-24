b0a78f2268126917851405cbb2ba5420602601ec
rykudev <86627645+rykudev@users.noreply.github.com>
2021-07-24 02:52:31 +0100

    Update gethidden and sethiddenproperty.hpp

--- gethidden and sethiddenproperty.hpp
+++ gethidden and sethiddenproperty.hpp
@@ -3,7 +3,7 @@ goto LABEL_35;                              // Goto not a valid property error
   if ( v7 < 0 )                                 // if failed
     goto LABEL_35;                              // Goto not a valid property error
   v8 = kname[v7];                               // Prop table (Nested)
-  v36 = *(_DWORD *)(*v3 + 12);
+  v36 = *(_DWORD *)(*v3 + 12);                  // find property
   v9 = (_DWORD *)rbx_get_property(v8);          // Get prop out of KName (nested table)
   if ( !v9 )
     goto LABEL_35;                              // Goto not a valid property error
